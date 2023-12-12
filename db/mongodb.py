7b5e84ac482748e66a3cfb8f4b3cbca7d5f92f11
carbarcha <samhoward13261@gmail.com>
2023-12-12 16:49:01 +0800

    add db

---
+++ db/mongodb.py
@@ -0,0 +1,6 @@
+
+def mongo_create_user():
+    pass
+
+def mongo_get_user():
+    pass
---
+++ db/weaviate.py
@@ -0,0 +1,9 @@
+
+def weaviate_create_user():
+    pass
+
+def weaviate_get_user():
+    pass
+
+def weaviate_vector_search():
+    pass
